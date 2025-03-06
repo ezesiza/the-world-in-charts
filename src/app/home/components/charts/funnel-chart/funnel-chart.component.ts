b9dcb143362e687f6e783693a7e826a2f6312be7
ezesiza <ezesiza@gmail.com>
2025-03-06 22:25:57 +0100

    setup project structure

---
+++ src/app/home/components/charts/funnel-chart/funnel-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-funnel-chart',
+  templateUrl: './funnel-chart.component.html',
+  styleUrls: ['./funnel-chart.component.less']
+})
+export class FunnelChartComponent {
+
+}
